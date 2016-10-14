4a5ec800ba7eac71e3b1d06a2bf39bef2a94abef
Tim Lewis <timothy.jaron.lewis@gmail.com>
2016-10-13 20:35:56 -0600

    Added gulpfile. Added watches to copy content to /wamp/www for development.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('css', function () {
+    // place code for your default task here
+    return watch('css/**/*.css', { ignoreInitial: false })
+    .pipe(gulp.dest('/wamp/www/css'));
+});
+
+gulp.task('js', function () {
+    // place code for your default task here
+    return watch('js/**/*.js', { ignoreInitial: false })
+    .pipe(gulp.dest('/wamp/www/js'));
+});
+
+gulp.task('fonts', function () {
+    // place code for your default task here
+    return watch('fonts/**/*.*', { ignoreInitial: false })
+    .pipe(gulp.dest('/wamp/www/fonts'));
+});
+
+gulp.task('html', function () {
+    // place code for your default task here
+    return watch('*.html', { ignoreInitial: false })
+    .pipe(gulp.dest('/wamp/www'));
+});
